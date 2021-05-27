a14a22369c33b0d1314c1072e1af4a11c945162c
Dawon Gregory <dawongregory@gmail.com>
2021-05-26 21:48:55 -0600

    Created login and registration with Bruce image. Updated models

--- RememberBruce/Models/LoginUser.cs
+++ RememberBruce/Models/LoginUser.cs
@@ -1,18 +1,23 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace RememberBruce.Models
 {
+    [NotMapped]///DON'T ADD TO DATABASE\\\
     public class LoginUser
     {
-        [Required]
+        //////////LOGGED IN USER'S EMAIL\\\\\\\\\\\\
+        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "Email:")]
         public string LoginEmail { get; set; }
 
-        [Required]
+        //////////LOGGED IN USER'S PASSWORD\\\\\\\\\\\\
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(8, ErrorMessage = "at least 8 characters.")]
         [DataType(DataType.Password)]
-        [MinLength(8, ErrorMessage = "Password must be 8 characters or longer!")]
-        [Display(Name = "Password")]
+        [Display(Name = "Password:")]
         public string LoginPassword { get; set; }
     }
 }
